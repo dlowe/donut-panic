3783e2b6979ca9e1a8c2ff34735ecfb86b550a59
J. David Lowe <j.david.lowe@gmail.com>
2013-08-24 14:01:23 -0700

    simple drawing

--- client/game.js
+++ client/game.js
@@ -1,18 +1,76 @@
 var game = (function () {
+    var ctx;
     var ws;
     var States = {
         UNOPENED: 0,
-        READY: 1,
-        STARTED: 2
+        MAZE_WAIT: 1,
+        PLAYER_WAIT: 2,
+        STARTED: 3
     };
     var state = States.UNOPENED;
+    var maze = {
+        height: null,
+        width: null,
+        walls: null,
+    };
+    var player = {
+        x: null,
+        y: null,
+    };
+    var entities = null;
+    var sprites = {
+        "wall": new Image(),
+        "player": new Image(),
+    }
+    sprites.wall.src = "wall.png";
+    sprites.player.src = "player.png";
+
+    var repaint = function(timestamp) {
+        for (var x = 0; x < maze.width; ++x) {
+            for (var y = 0; y < maze.height; ++y) {
+                if (maze.walls[x][y]) {
+                    ctx.drawImage(sprites.wall, x*64, y*64, 64, 64);
+                }
+            }
+        }
+        ctx.drawImage(sprites.player, player.x*64, player.y*64, 64, 64);
+        requestAnimationFrame(repaint);
+    }
 
     var message = function(msg) {
         switch (state) {
-            case States.READY:
-                if (msg.lastIndexOf('start:', 0) === 0) {
-                    ws.send("started");
+            case States.MAZE_WAIT:
+                var maze_pattern = /^maze:\s+(\d+)\s+(\d+)\s+(.*)$/;
+                if (result = maze_pattern.exec(msg)) {
+                    console.log(result);
+                    maze.width = parseInt(result[1]);
+                    maze.height = parseInt(result[2]);
+                    maze.walls = [];
+                    for (var x = 0; x < maze.width; ++x) {
+                        maze.walls[x] = [];
+                    }
+                    for (var y = 0; y < maze.height; ++y) {
+                        for (var x = 0; x < maze.width; ++x) {
+                            maze.walls[x][y] = result[3].charAt(y * maze.width + x) == 'x';
+                        }
+                    }
+                    console.log(maze.walls);
+                    ws.send("maze_ack");
+                    state = States.PLAYER_WAIT;
+                } else {
+                    throw "wtf?";
+                }
+                break;
+            case States.PLAYER_WAIT:
+                var player_pattern = /^player:\s+(\d+)\s+(\d+)$/;
+                if (result = player_pattern.exec(msg)) {
+                    console.log(result);
+                    player.x = parseInt(result[1]);
+                    player.y = parseInt(result[2]);
+                    ws.send("player_ack");
                     state = States.STARTED;
+                    // start painting!
+                    requestAnimationFrame(repaint);
                 } else {
                     throw "wtf?";
                 }
@@ -25,11 +83,12 @@ var game = (function () {
     }
 
     return {
-        start: function(host, port, cookie) {
+        start: function(host, port, cookie, canvas_context) {
+                   ctx = canvas_context;
                    ws = new WebSocket("ws://" + host + ":" + port + "/play-game/" + cookie);
                    ws.onopen = function() {
                        ws.send("ready");
-                       state = States.READY;
+                       state = States.MAZE_WAIT;
                    }
                    ws.onmessage = function(event) {
                        message(event.data);
--- main.py
+++ main.py
@@ -33,8 +33,9 @@ class JoinGame(tornado.web.RequestHandler):
 class PlayGame(tornado.websocket.WebSocketHandler):
     class States:
         OPEN = 0
-        READY = 1
-        STARTED = 2
+        MAZE_SENT = 1
+        PLAYER_SENT = 2
+        STARTED = 3
 
     def open(self, cookie):
         self.cookie = cookie
@@ -43,19 +44,31 @@ class PlayGame(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         print "[%s] new message %s" % (self.cookie, message)
-        if message == 'ready':
-            if self.state == self.States.OPEN:
-                self.write_message('start: 2 2 xxxx')
-                self.state = self.States.READY
+        if self.state == self.States.OPEN:
+            if message == 'ready':
+                self.write_message('maze: 5 4 ' + ''.join(
+                    ['xxxxx',
+                     'x   x',
+                     'x   x',
+                     'xxxxx',]))
+                self.state = self.States.MAZE_SENT
             else:
                 raise Exception('wtf?')
-        elif message == 'started':
-            if self.state == self.States.READY:
+        elif self.state == self.States.MAZE_SENT:
+            if message == 'maze_ack':
+                self.write_message('player: 2 1')
+                self.state = self.States.PLAYER_SENT
+            else:
+                raise Exception('wtf?')
+        elif self.state == self.States.PLAYER_SENT:
+            if message == 'player_ack':
                 self.state = self.States.STARTED
             else:
                 raise Exception('wtf?')
-        else:
+        elif self.state == self.States.STARTED:
             self.write_message(u"You said: " + message)
+        else:
+            raise Exception('wtf?')
 
 if __name__ == "__main__":
     handlers = [
