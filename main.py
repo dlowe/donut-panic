66ea0191489d6ce0b946ac582f910a4b47d032a8
J. David Lowe <j.david.lowe@gmail.com>
2013-08-23 22:28:47 -0700

    ...

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class NewGame(tornado.web.RequestHandler):
 
 class JoinGame(tornado.web.RequestHandler):
     def post(self, game_id):
+        print "join game"
         self.set_header("Content-Type", "application/json")
         self.write({
             "host": tornado.options.options.host,
@@ -24,12 +25,31 @@ class JoinGame(tornado.web.RequestHandler):
         })
 
 class PlayGame(tornado.websocket.WebSocketHandler):
-    def open(self, thingy):
-        print "new connection %s" % thingy
+    class States:
+        OPEN = 0
+        READY = 1
+        STARTED = 2
+
+    def open(self, cookie):
+        self.cookie = cookie
+        self.state = self.States.OPEN
+        print "[%s] new connection" % cookie
 
     def on_message(self, message):
-        print "new message %s" % message
-        self.write_message(u"You said: " + message)
+        print "[%s] new message %s" % (self.cookie, message)
+        if message == 'ready':
+            if self.state == self.States.OPEN:
+                self.write_message('start: 2 2 xxxx')
+                self.state = self.States.READY
+            else:
+                raise Exception('wtf?')
+        elif message == 'started':
+            if self.state == self.States.READY:
+                self.state = self.States.STARTED
+            else:
+                raise Exception('wtf?')
+        else:
+            self.write_message(u"You said: " + message)
 
 if __name__ == "__main__":
     handlers = [
