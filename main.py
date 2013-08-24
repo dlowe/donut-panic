4ffb4516c51c6bcd3120011f99228df891fa9445
J. David Lowe <j.david.lowe@gmail.com>
2013-08-24 16:59:30 -0700

    make objects out of players and games...

--- main.py
+++ main.py
@@ -8,38 +8,28 @@ from tornado.options import define
 define("host", default="localhost", help="host", type=str)
 define("port", default=5000, help="port", type=int)
 
-class Index(tornado.web.RequestHandler):
-    def get(self):
-        self.render("client/index.html")
-
 GAMES = {}
 
-def game_tick(game_id):
-    ## move everything
-    for player in GAMES[game_id]["players"].values():
-        if player["right?"]:
-            player["x"] += 0.03
-        if player["up?"]:
-            player["y"] -= 0.03
-        if player["left?"]:
-            player["x"] -= 0.03
-        if player["down?"]:
-            player["y"] += 0.03
-    ## send updates
-    for player in GAMES[game_id]["players"].values():
-        player["socket"].maybe_send_player()
+class Player:
+    def __init__(self, player_id):
+        self.x = 2.0
+        self.y = 2.0
+        self.width = 32
+        self.height = 32
+        self.up = False
+        self.down = False
+        self.left = False
+        self.right = False
+        self.socket = None
 
-class NewGame(tornado.web.RequestHandler):
-    def post(self):
-        print "new game"
-        game_id = "testing" ## XXX: randomize
-        GAMES[game_id] = {
-            "players": {},
-            "tick": lambda: game_tick(game_id),
-            "started": False,
-            "width": 20,
-            "height": 20,
-            "maze": "".join( ## XXX: randomize
+class Game:
+    def __init__(self, game_id):
+        self.game_id = game_id
+        self.players = {}
+        self.started = False
+        self.width = 20
+        self.height = 20
+        self.maze = "".join( ## XXX: randomize
                     ['xxxxxxxxxxxxxxxxxxxx',
                      'x                  x',
                      'x                  x',
@@ -60,26 +50,43 @@ class NewGame(tornado.web.RequestHandler):
                      'x                  x',
                      'x                  x',
                      'xxxxxxxxxxxxxxxxxxxx',])
-        }
+
+    def tick(self):
+        ## move everything
+        for player in self.players.values():
+            if player.right:
+                player.x += 0.03
+            if player.up:
+                player.y -= 0.03
+            if player.left:
+                player.x -= 0.03
+            if player.down:
+                player.y += 0.03
+        ## send updates
+        for player in self.players.values():
+            player.socket.maybe_send_player()
+
+    def add_player(self, player_id):
+        self.players[player_id] = Player(player_id)
+
+    def get_player(self, player_id):
+        return self.players[player_id]
+
+class NewGameHandler(tornado.web.RequestHandler):
+    def post(self):
+        print "new game"
+        game_id = "testing" ## XXX: randomize
+        GAMES[game_id] = Game(game_id)
         self.set_header("Content-Type", "application/json")
         self.write({
-            "game_id": "testing",
+            "game_id": game_id,
         })
 
-class JoinGame(tornado.web.RequestHandler):
+class JoinGameHandler(tornado.web.RequestHandler):
     def post(self, game_id):
         print "[%s] join game" % game_id
         player_id = "cookie" ## XXX: randomize
-        GAMES[game_id]["players"][player_id] = {
-            ## XXX: randomize
-            "x": 2.0,
-            "y": 2.0,
-            "up?": False,
-            "down?": False,
-            "left?": False,
-            "right?": False,
-            "socket": None,
-        }
+        GAMES[game_id].add_player(player_id)
         self.set_header("Content-Type", "application/json")
         self.write({
             "host": tornado.options.options.host,
@@ -88,7 +95,7 @@ class JoinGame(tornado.web.RequestHandler):
             "player_id": player_id
         })
 
-class PlayGame(tornado.websocket.WebSocketHandler):
+class PlayGameSocket(tornado.websocket.WebSocketHandler):
     class States:
         OPEN = 0
         MAZE_SENT = 1
@@ -98,58 +105,63 @@ class PlayGame(tornado.websocket.WebSocketHandler):
     def open(self, game_id, player_id):
         self.game_id = game_id
         self.player_id = player_id
+        self.game = GAMES[game_id]
+        self.player = self.game.get_player(player_id)
+
         self.state = self.States.OPEN
-        GAMES[game_id]["players"][player_id]["socket"] = self
+        self.player.socket = self
         print "[%s/%s] new connection" % (self.game_id, self.player_id)
 
     def maybe_send_player(self):
         if self.state == self.States.ACKED:
-            self.write_message('player: %f %f' % (GAMES[self.game_id]["players"][self.player_id]["x"],
-                GAMES[self.game_id]["players"][self.player_id]["y"]))
+            self.write_message('player: %f %f' % (self.player.x, self.player.y))
             self.state = self.States.ACK_WAIT
 
     def on_message(self, message):
         # print "[%s/%s] new message %s" % (self.game_id, self.player_id, message)
         if self.state == self.States.OPEN:
             if message == 'ready':
-                self.write_message('maze: %d %d %s' % (GAMES[self.game_id]["width"],
-                    GAMES[self.game_id]["height"],
-                    GAMES[self.game_id]["maze"]))
+                self.write_message('maze: %d %d %s' % (self.game.width,
+                    self.game.height, self.game.maze))
                 self.state = self.States.MAZE_SENT
         elif self.state == self.States.MAZE_SENT:
             if message == 'ack':
                 self.state = self.States.ACKED
-                if not GAMES[self.game_id]["started"]:
-                    tornado.ioloop.PeriodicCallback(GAMES[self.game_id]["tick"], 16).start()
-                    GAMES[self.game_id]["started"] = True
+                if not self.game.started:
+                    tornado.ioloop.PeriodicCallback(lambda: self.game.tick(), 16).start()
+                    self.game.started = True
         elif self.state == self.States.ACK_WAIT:
             if message == 'ack':
                 self.state = self.States.ACKED
 
         if self.state == self.States.ACK_WAIT or self.state == self.States.ACKED:
             if message == 'right':
-                GAMES[self.game_id]["players"][self.player_id]["right?"] = True
+                self.player.right = True
             if message == '!right':
-                GAMES[self.game_id]["players"][self.player_id]["right?"] = False
+                self.player.right = False
             if message == 'left':
-                GAMES[self.game_id]["players"][self.player_id]["left?"] = True
+                self.player.left = True
             if message == '!left':
-                GAMES[self.game_id]["players"][self.player_id]["left?"] = False
+                self.player.left = False
             if message == 'down':
-                GAMES[self.game_id]["players"][self.player_id]["down?"] = True
+                self.player.down = True
             if message == '!down':
-                GAMES[self.game_id]["players"][self.player_id]["down?"] = False
+                self.player.down = False
             if message == 'up':
-                GAMES[self.game_id]["players"][self.player_id]["up?"] = True
+                self.player.up = True
             if message == '!up':
-                GAMES[self.game_id]["players"][self.player_id]["up?"] = False
+                self.player.up = False
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("client/index.html")
 
 if __name__ == "__main__":
     handlers = [
-        (r"/", Index),
-        (r"/api/new-game", NewGame),
-        (r"/api/join-game/(.*)", JoinGame),
-        (r"/play-game/([^/]+)/(.*)", PlayGame),
+        (r"/", IndexHandler),
+        (r"/api/new-game", NewGameHandler),
+        (r"/api/join-game/(.*)", JoinGameHandler),
+        (r"/play-game/([^/]+)/(.*)", PlayGameSocket),
         (r"/(.*)", tornado.web.StaticFileHandler, {"path": './client/'}),
     ]
 
